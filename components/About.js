ba43aa0801213cee776cdbacc5f9594688376ecf
demureaxs <118724010+Demureaxs@users.noreply.github.com>
2023-02-20 13:07:22 +0700

    broken links fixed

--- components/About.js
+++ components/About.js
@@ -28,7 +28,7 @@ function About() {
       >
         <motion.img
           className="absolute -top-52 h-[700px] w-[700px] xl:h-full xl:w-full xl:top-0 object-cover "
-          src="https://scontent.fdps2-1.fna.fbcdn.net/v/t39.30808-6/241049583_10218495370885442_1472116862498089112_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGRE2wFoj3bUxcQXd5_kotyP_pWhbTaT_k_-laFtNpP-RiM3YCPCrLN5id1G2bc50Y&_nc_ohc=w-bLMVdVgHoAX8EjGIS&tn=M9CzQmuy2-WAsl_a&_nc_ht=scontent.fdps2-1.fna&oh=00_AfAuN0O2eS6djiCIwD-YjDKp_NXbg6EOmXJdAnneTLiXCA&oe=63F1289E"
+          src="https://scontent.fdps2-1.fna.fbcdn.net/v/t39.30808-6/241049583_10218495370885442_1472116862498089112_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGRE2wFoj3bUxcQXd5_kotyP_pWhbTaT_k_-laFtNpP-RiM3YCPCrLN5id1G2bc50Y&_nc_ohc=j_ptBe_IxPMAX-FJGlS&_nc_ht=scontent.fdps2-1.fna&oh=00_AfAOST7DB3f_BzJ6spuPQP6HorJlncll9nSn1b2sZHryIg&oe=63F7175E"
         />
       </motion.div>
 
